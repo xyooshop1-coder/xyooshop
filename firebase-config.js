52049eceb79ee585acdfaf1eaecce1e9e7a2f829
xyooshop1-coder <xyooshop1@gmail.com>
2025-08-15 17:18:25 +0800

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,13 @@
 // Replace with your Firebase project settings
-const firebaseConfig = {
-  apiKey: "AIzaSyDQXDOxP_XSt8PWux2ipaKXQG_EzxE9W48",
-  authDomain: "xyoo-shop.firebaseapp.com",
-  projectId: "xyoo-shop",
-  storageBucket: "xyoo-shop.firebasestorage.app",
-  messagingSenderId: "671592025442",
-  appId: "1:671592025442:web:a5ce853dd6426affb2b03b",
-  measurementId: "G-S4PVBV4KQS"
-};
+ const firebaseConfig = {
+    apiKey: "AIzaSyDQXDOxP_XSt8PWux2ipaKXQG_EzxE9W48",
+    authDomain: "xyoo-shop.firebaseapp.com",
+    projectId: "xyoo-shop",
+    storageBucket: "xyoo-shop.firebasestorage.app",
+    messagingSenderId: "671592025442",
+    appId: "1:671592025442:web:41f8b45661090238b2b03b",
+    measurementId: "G-NW28KNMRLS"
+  };
 
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
